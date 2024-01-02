fb9ae73b1c4f271f6ebde814beb89ab87ae68b48
jueun oh <jueun1025@naver.com>
2024-01-02 22:19:39 +0900

    feat : 각 페이지의 큰 수 구하기

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -16,6 +16,10 @@ class Problem1 {
         if (checkOdd(pobiLeftPage) || checkEven(pobiRightPage) || checkOdd(crongLeftPage) || checkEven(crongRightPage)) {
             return -1;
         }
+        int pobiLeftMax = selectMaxBetweenSumAndMul(getSum(pobiLeftPage), getMul(pobiLeftPage));
+        int pobiRightMax = selectMaxBetweenSumAndMul(getSum(pobiRightPage), getMul(pobiRightPage));
+        int crongLeftMax = selectMaxBetweenSumAndMul(getSum(crongLeftPage), getMul(crongLeftPage));
+        int crongRightMax = selectMaxBetweenSumAndMul(getSum(crongRightPage), getMul(crongRightPage));
 
         int answer = Integer.MAX_VALUE;
         return answer;
@@ -28,5 +32,31 @@ class Problem1 {
     public static boolean checkEven(int page) { // 짝수 확인
         return (page % 2) == 0;
     }
+
+    public static int selectMaxBetweenSumAndMul(int sum, int mul) {
+        return Math.max(sum, mul);
+    }
+
+    public static int getSum(int page) {
+        int sum = 0;
+        for (int i = 2; i >= 0; i--) {
+            int digit = (int) (page / Math.pow(10, i));
+            sum += digit;
+            page %= 10;
+        }
+        return sum;
+    }
+
+    public static int getMul(int page) {
+        int mul = 1;
+        for (int i = 2; i >= 0; i--) {
+            int digit = (int) (page / Math.pow(10, i));
+            digit = (digit == 0) ? 1 : digit;
+            mul *= digit;
+            page %= 10;
+        }
+        return mul;
+    }
+
 
 }

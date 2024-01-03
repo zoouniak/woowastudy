254394e8a2e19d991b4ea008c799eab86ae6506c
jueun oh <jueun1025@naver.com>
2024-01-04 00:32:27 +0900

    feat : 중복 문자 제거

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,22 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String decode = removeDup(cryptogram);
+        while (!decode.equals(cryptogram)) {
+            cryptogram = decode;
+            decode = removeDup(cryptogram);
+        }
+
+        String answer = cryptogram;
         return answer;
+    }
+
+    public static String removeDup(String c) {
+        for (int i = 0; i < c.length() - 1; i++) {
+            if (c.charAt(i) == c.charAt(i + 1)) {
+                return c.substring(0, i) + c.substring(i + 2);
+            }
+        }
+        return c;
     }
 }

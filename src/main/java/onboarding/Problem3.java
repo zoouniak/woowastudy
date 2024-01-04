69d7af45cd8ba763abf64b57919402b7cbe581cf
jueun oh <jueun1025@naver.com>
2024-01-05 02:04:32 +0900

    feat : 박수 횟수 구하기

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+                answer+= clap(i);
+        }
         return answer;
+    }
+
+    public static int clap(int target) {
+        int count=0;
+        while (target > 0) {
+            if (target % 10 == 3 || target % 10 == 6 || target % 10 == 9) {
+                count++;
+            }
+            target /= 10;
+        }
+        return count;
     }
 }

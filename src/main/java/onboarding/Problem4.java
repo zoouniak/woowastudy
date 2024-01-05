46ebe421842c1e846504a591b8f886c8f1add4ce
jueun oh <jueun1025@naver.com>
2024-01-06 00:41:52 +0900

    feat : 알파벳 변환

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,19 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            sb.append(invert(word.charAt(i)));
+        }
+        return sb.toString();
+    }
+
+    public static String invert(Character c) {
+        if (c >= 'A' && c <= 'Z') {
+            return String.valueOf((char) (155 - (int) c));
+        } else if (c >= 'a' && c <= 'z') {
+            return String.valueOf((char) (219 - (int) c));
+        } else return String.valueOf(c);
     }
 }

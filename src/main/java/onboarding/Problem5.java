5d98fc23f4247637de6bd1055682ba72d6d25ad7
jueun oh <jueun1025@naver.com>
2024-01-07 00:07:12 +0900

    feat : 잔돈 최소 개수 구하기

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,19 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        int[] change = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        List<Integer> answer = new ArrayList<>();
+
+        if (money >= 1 && money <= 1000000) {
+            for (int i = 0; i < 9; i++) {
+                answer.add(money / change[i]);
+                money %= change[i];
+            }
+        }
         return answer;
     }
 }

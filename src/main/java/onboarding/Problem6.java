8c989b90d497743de656255487d379e0703b57ec
jueun oh <jueun1025@naver.com>
2024-01-07 15:18:36 +0900

    feat : 이메일 제한사항

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -5,6 +5,16 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
+        for (List<String> form : forms) {
+            String email = form.get(0);
+            if (email.length() < 11 || email.length() > 20) // 길이 제한
+                return null;
+            if (email.split("@")[1].equals("email.com")) // 형식 제한
+                return null;
+        }
+
+
         return answer;
     }
+
 }
